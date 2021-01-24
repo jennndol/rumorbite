47d8d6af9bf30085f6e6c06ddd11fac58fe85f48
Luthfi <jkt.luthfi@gmail.com>
2021-01-24 18:28:13 +0700

    refactoring

--- src/common/utils/password.ts
+++ src/common/utils/password.ts
@@ -9,13 +9,13 @@ export function sign(password: string, salt: string): string {
   const hmac = crypto.createHmac(process.env.HASH_ALGORITHM, salt);
   hmac.update(password);
   const hash = hmac.digest('hex');
-  return `${salt}:${hash}`;
+  return `${salt}${hash}`;
 }
 
 export function compare(password: string, signature: string): boolean {
-  const [salt] = signature.split(':');
+  const salt = signature.slice(0, parseInt(process.env.HASH_SALT_ROUNDS));
   const passwordData = sign(password, salt);
-  if (passwordData.split(':')[1] === signature.split(':')[1]) {
+  if (passwordData.slice(50) === signature.slice(50)) {
     return true;
   }
   return false;
